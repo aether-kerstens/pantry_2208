e7e9d9707b10b906f30a7286a6282ba24c70b4ea
Aether Kerstens <philippekerstens@gmail.com>
2022-09-27 08:42:52 -0700

    Complete iteration 1

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,9 +1,11 @@
 class Ingredient
-  attr_reader :name, :unit, :calories
+  
+  attr_reader :name, :unit, :calories, :amount
 
   def initialize(ingredient_data)
     @name = ingredient_data[:name]
     @unit = ingredient_data[:unit]
     @calories = ingredient_data[:calories]
+    @amount = ingredient_data[:amount]
   end
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -4,16 +4,20 @@ RSpec.describe Ingredient do
   before(:each) do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
   end
+  
   describe '#initialize' do
     it 'can create ingredient' do
       expect(@ingredient1).to be_an_instance_of(Ingredient)
     end
+
     it 'can have name' do
       expect(@ingredient1.name).to eq("Cheese")
     end
+
     it 'can have a unit of measurement' do
       expect(@ingredient1.unit).to eq("oz")
     end
+
     it 'can have calories' do
       expect(@ingredient1.calories).to eq(50)
     end
