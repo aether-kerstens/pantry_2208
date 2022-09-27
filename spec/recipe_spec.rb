df116b080080023543e2f2b70ef3bbcaf88c0a5c
Aether Kerstens <philippekerstens@gmail.com>
2022-09-27 10:23:55 -0700

    Write test for Total Calories method

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,22 +6,42 @@ RSpec.describe Recipe do
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
   end
-  it 'can have a name' do
-    expect(@recipe1.name).to eq("Mac and Cheese")
+  describe '#initialize' do
+    it 'can have a name' do
+      expect(@recipe1.name).to eq("Mac and Cheese")
+    end
+    it 'can require ingredients' do
+      expect(@recipe1.ingredients_required).to eq({})
+    end
+    it 'can add ingredients' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
+    end
+    xit 'can call on ingredients' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+    end
   end
-  it 'can require ingredients' do
-    expect(@recipe1.ingredients_required).to eq({})
-  end
-  it 'can add ingredients' do
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
-    expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
-  end
-  xit 'can call on ingredients' do
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
-    expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+  describe '#total_calories' do
+    it 'can sum total calories' do
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@recipe1.total_calories).to eq(440)
+
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe2 = Recipe.new("Cheese Burger")
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+      expect(@recipe2.total_calories).to eq(675)
+    end
   end
 end
